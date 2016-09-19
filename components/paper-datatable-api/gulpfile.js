86078b4aaaf712a5edb0ee570d58eb1a339b2593
Julien ROUSSEAU <jrousseau@doubletrade.com>
2016-09-19 21:07:46 +0200

    Update doc

---
+++ components/paper-datatable-api/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('build', () => {
+  gulp.src('src/*.js')
+  .pipe(babel({
+    presets: ['es2015'],
+  }))
+  .pipe(gulp.dest('./dist'));
+
+  gulp.src('src/*.html')
+  .pipe(gulp.dest('./dist'));
+});
---
+++ components/paper-datatable-api/src/paper-datatable-api-column.js
@@ -0,0 +1,109 @@
+class DtPaperDatatableApiColumn {
+
+  beforeRegister() {
+    this.is = 'paper-datatable-api-column';
+
+    this.properties = {
+      /**
+       * The name of the header for this column.
+       */
+      header: String,
+      /**
+       * Property which will be available in data (throught paper-datatable-api).
+       */
+      property: String,
+      /**
+       * If set, these other properties will be available in data (throught paper-datatable-api).
+       * Usefull to keep id of the object.
+       */
+      otherProperties: {
+        type: Array,
+        value: [],
+      },
+      /**
+       * If true, the colum can be sort.
+       */
+      sortable: {
+        type: Boolean,
+        value: false,
+      },
+      /**
+       * If false, the column is hidden.
+       */
+      show: {
+        type: Boolean,
+        value: true,
+      },
+      /**
+       * Current sort direction (asc or desc).
+       */
+      sortDirection: String,
+      /**
+       * If true, the column is currently sorted.
+       */
+      sorted: {
+        type: Boolean,
+        value: false,
+      },
+      /**
+       * If true, the column can be filtered.
+       */
+      filter: {
+        type: Boolean,
+        value: false,
+      },
+      /**
+       * If true, a date picker is displayed when the column is sorted.
+       */
+      date: {
+        type: Boolean,
+        value: false,
+      },
+      /**
+       * If true, the column can be hide.
+       */
+      hideable: {
+        type: Boolean,
+        value: false,
+      },
+      /**
+       * Position of the column in the table.
+       */
+      position: Number,
+      activeFilter: {
+        type: Boolean,
+        value: false,
+      },
+    };
+  }
+
+  get behaviors() {
+    return [
+      Polymer.Templatizer,
+    ];
+  }
+
+  ready() {
+    const props = {};
+    props.__key__ = true;
+    props[this.header] = true;
+    props[this.property] = true;
+    this._instanceProps = props;
+
+    const template = Polymer.dom(this).querySelector('template');
+    this.templatize(template);
+  }
+
+  /**
+   * Stamp the value in template (according to property).
+   *
+   * @property fillTemplate
+   * @param {String} value The value of the property.
+   */
+  fillTemplate(value, otherValues) {
+    const instance = this.stamp({ value, otherValues });
+    return instance;
+  }
+}
+
+Polymer(DtPaperDatatableApiColumn);
